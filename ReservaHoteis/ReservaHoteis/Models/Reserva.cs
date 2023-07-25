f21e0dbc16dca4edba9b92d8996cf7c553ac0894
Artur D Angelo <arturdangelo@gmail.com>
2023-07-24 22:52:55 -0300

    Desafio do Projeto - Reserva de Hotéis - Web Dio

---
+++ ReservaHoteis/ReservaHoteis/Models/Reserva.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservaHoteis.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {     
+            if (Suite.Capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new ArgumentException("A capacidade da suíte é menor do que a quantidade de hóspedes");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+
+            decimal valor = Suite.ValorDiaria * DiasReservados;
+
+            if (DiasReservados >= 10)
+            {
+                valor = valor - (valor/10);
+            }
+
+            return valor;
+        }
+    }
+}
